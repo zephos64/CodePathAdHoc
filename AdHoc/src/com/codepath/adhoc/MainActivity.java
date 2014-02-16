3bfd18326f76596fca99a22b50bde85f6b2c7625
ryan <ryanwhitworth@sbcglobal.net>
2014-02-16 15:05:09 -0800

    -now auto-login into facebook if have a valid session open

--- AdHoc/src/com/codepath/adhoc/MainActivity.java
+++ AdHoc/src/com/codepath/adhoc/MainActivity.java
@@ -1,8 +1,8 @@
 package com.codepath.adhoc;
 
-import java.util.Arrays;
 import java.util.List;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -10,67 +10,43 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
+import com.codepath.adhoc.application.FacebookClient;
+import com.facebook.Request;
+import com.facebook.Response;
 import com.parse.FindCallback;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseFacebookUtils;
-import com.parse.ParseFacebookUtils.Permissions;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 import com.parse.SignUpCallback;
 
-public class MainActivity extends FragmentActivity {	
+public class MainActivity extends FragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-		
-//TODO read add these, remove because cleaning whenever I modify this page got annoying
-//		ActionBar actionBar = getActionBar();
-//		actionBar.hide();
 
-		parseUserSignup();
-		parseUserLogin();
-		
+		// TODO uncomment, commenting out because requires cleaning every time
+		ActionBar actionBar = getActionBar();
+		actionBar.hide();
+
+		//parseUserSignup();
+		//parseUserLogin();
+
 		ParseFacebookUtils.initialize(getString(R.string.app_id));
-	}
-	
-	public void test(View v) {
-		Log.e("err", "testing 2");
-		ParseFacebookUtils.logIn(this, new LogInCallback() {
+		FacebookClient.getFacebookUser(new Request.Callback() {
 			@Override
-			public void done(ParseUser user, ParseException err) {
-				if (user == null) {
-					Log.d("debug",
-							"Uh oh. The user cancelled the Facebook login.");
-				} else {
-					Log.d("debug", "User logged in through Facebook!");
-
-					final ParseUser userLogin;
-					userLogin = user;
-					if (!ParseFacebookUtils.isLinked(userLogin)) {
-						ParseFacebookUtils.link(userLogin, getParent(),
-								new SaveCallback() {
-									@Override
-									public void done(ParseException ex) {
-										if (ParseFacebookUtils
-												.isLinked(userLogin)) {
-											Log.d("debug",
-													"Account linked with ParseUser acc");
-										}
-									}
-								});
-					}
-
-					Intent i = new Intent(getApplicationContext(),
-							EventListActivity.class);
-					startActivity(i);
+			public void onCompleted(Response response) {
+				Log.d("debug", "Facebook user sesssion errors: " + response.getError());
+				if(response.getError() == null) {
+					loginFacebook();
 				}
 			}
-		});
+	    });
 	}
-	
+
 	public void parseUserSignup() {
 		// Create the ParseUser
 		ParseUser user = new ParseUser();
@@ -117,18 +93,18 @@ public class MainActivity extends FragmentActivity {
 			// show the signup or login screen
 		}
 	}
-	
+
 	public void queryUsers() {
 		ParseQuery<ParseUser> query = ParseUser.getQuery();
 		query.whereGreaterThan("age", 20); // find adults
 		query.findInBackground(new FindCallback<ParseUser>() {
-		  public void done(List<ParseUser> objects, ParseException e) {
-		    if (e == null) {
-		        // The query was successful.
-		    } else {
-		        // Something went wrong.
-		    }
-		  }
+			public void done(List<ParseUser> objects, ParseException e) {
+				if (e == null) {
+					// The query was successful.
+				} else {
+					// Something went wrong.
+				}
+			}
 		});
 	}
 
@@ -137,5 +113,41 @@ public class MainActivity extends FragmentActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void loginFacebook() {
+		ParseFacebookUtils.logIn(this, new LogInCallback() {
+			@Override
+			public void done(final ParseUser user, ParseException err) {
+				if (user == null) {
+					Log.d("debug",
+							"Uh oh. The user cancelled the Facebook login.");
+				} else {
+					Log.d("debug", "User logged in through Facebook!");
+
+					if (!ParseFacebookUtils.isLinked(user)) {
+						ParseFacebookUtils.link(user, getParent(),
+								new SaveCallback() {
+									@Override
+									public void done(ParseException ex) {
+										if (ParseFacebookUtils.isLinked(user)) {
+											Log.d("debug",
+													"Account linked with ParseUser acc");
+										}
+									}
+								});
+					}
+
+					Intent i = new Intent(getApplicationContext(),
+							EventListActivity.class);
+					startActivity(i);
+				}
+			}
+		});
+	}
+
+	public void loginWithParse(View view) {
+		Log.d("debug", "Login To Rest Clicked");
+		loginFacebook();
 	}
 }
--- AdHoc/src/com/codepath/adhoc/application/FacebookClient.java
+++ AdHoc/src/com/codepath/adhoc/application/FacebookClient.java
@@ -1,33 +1,20 @@
 package com.codepath.adhoc.application;
 
-import org.scribe.builder.api.Api;
-import org.scribe.builder.api.FacebookApi;
+import com.facebook.HttpMethod;
+import com.facebook.Request;
+import com.facebook.Request.Callback;
+import com.facebook.Response;
+import com.parse.ParseFacebookUtils;
 
-import android.content.Context;
-
-import com.codepath.oauth.OAuthBaseClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
-
-public class FacebookClient extends OAuthBaseClient {
-    public static final Class<? extends Api> REST_API_CLASS = FacebookApi.class;
-    public static final String REST_URL = "http://api.twitter.com";
-    public static final String REST_CONSUMER_KEY = "SOME_KEY_HERE";
-    public static final String REST_CONSUMER_SECRET = "SOME_SECRET_HERE";
-    public static final String REST_CALLBACK_URL = "oauth://arbitraryname.com";
-
-    public FacebookClient(Context context) {
-        super(context, REST_API_CLASS, REST_URL,
-          REST_CONSUMER_KEY, REST_CONSUMER_SECRET, REST_CALLBACK_URL);
-    }
-
-    // ENDPOINTS BELOW
-
-    public void getHomeTimeline(int page, AsyncHttpResponseHandler handler) {
-      String apiUrl = getApiUrl("statuses/home_timeline.json");
-      RequestParams params = new RequestParams();
-      params.put("page", String.valueOf(page));
-      client.get(apiUrl, params, handler);
-    }
-
+public class FacebookClient {
+	
+	public static void getFacebookUser(Callback callback) {
+		new Request(
+			    ParseFacebookUtils.getSession(),
+			    "/me",
+			    null,
+			    HttpMethod.GET,
+			    callback
+			).executeAsync();
+	}
 }
