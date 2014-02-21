96cc11678b5e0b10a4e55248d183fd1a233b58ae
Hari Gollapalli <harikrishna_gp@yahoo.com>
2014-02-20 22:22:26 -0800

    Test commit.

--- AdHoc/src/com/codepath/adhoc/fragments/CreateEventMapActivity.java
+++ AdHoc/src/com/codepath/adhoc/fragments/CreateEventMapActivity.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 public class CreateEventMapActivity extends CreateEventFragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 	}
 
