a0607db8c9dfe75841f51eeab5fee6175fa2a91c
Brian Pham <tkiet082187@gmail.com>
2014-02-21 10:06:54 -0800

    Update EventListFragment.java

--- AdHoc/src/com/codepath/adhoc/fragments/EventListFragment.java
+++ AdHoc/src/com/codepath/adhoc/fragments/EventListFragment.java
@@ -2,38 +2,69 @@ package com.codepath.adhoc.fragments;
 
 import java.util.ArrayList;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import com.codepath.adhoc.R;
 import com.codepath.adhoc.application.EventsAdapter;
 import com.codepath.adhoc.parsemodels.Events;
 
-public class EventListFragment extends ListFragment {
+import eu.erikw.PullToRefreshListView;
+
+public class EventListFragment extends Fragment {
 	
-	ArrayList<Events> events = new ArrayList<Events>();
 	EventsAdapter eventsAdapter;
-	ListView lvEvents;
+	PullToRefreshListView lvEvents;
+	ArrayList<Events> events;
+	FragmentActivity activityListener;
+	
+	private View mContentView = null;
+	
+	@Override
+	public void onAttach(Activity activity) {
+		// TODO Auto-generated method stub
+		super.onAttach(activity);
+		this.activityListener = (FragmentActivity) activity;
+	}
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		events = new ArrayList<Events>();
+		eventsAdapter = new EventsAdapter(getActivity(), events);
+		//Log.d("DEBUG", "list of events: " + events.toString());
+		
 	}
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		View view = inflater.inflate(R.layout.fragment_item_event, container, false);
-		eventsAdapter = new EventsAdapter(getActivity(), events);
-		lvEvents = (ListView) view.findViewById(R.id.lvEvents);
-		lvEvents.setAdapter(eventsAdapter);
-		return view;
+		mContentView = inflater.inflate(R.layout.fragment_list, container, false);
+		this.lvEvents = (PullToRefreshListView) mContentView.findViewById(R.id.lvEvents);
+		Log.d("DEBUG", "adapter: " + eventsAdapter.toString());
+		//Log.d("DEBUG", "listview: " + lvEvents.toString());
+		//setListAdapter(getAdapter());
+		lvEvents.setAdapter(getAdapter());
+		//Log.d("DEBUG", "listview: " + lvEvents.toString());
+		return mContentView;
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(savedInstanceState);
+	}
+	
+	public EventsAdapter getAdapter() {
+		return eventsAdapter;
 	}
 
 }
